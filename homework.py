73716f0d8ddd39daa518ccae21a2d415a4fd2a76
Михаил Ренжин <renzhin@gmail.com>
2023-09-06 11:58:18 +0300

    Подключить переменные из env и добавить докстринги

--- homework.py
+++ homework.py
@@ -1,11 +1,13 @@
-...
+import os
+import time
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,22 +22,45 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
+    """
+    Проверяет доступность переменных окружения.
+    которые необходимы для работы программы.
+    """
     ...
 
 
 def send_message(bot, message):
+    """
+    Отправляет сообщение в Telegram чат.
+    Определяемый переменной окружения TELEGRAM_CHAT_ID.
+    Принимает на вход два параметра.
+    Экземпляр класса Bot и строку с текстом сообщения.
+    """
     ...
 
 
 def get_api_answer(timestamp):
+    """
+    Делает запрос к единственному эндпоинту API-сервиса.
+    В качестве параметра в функцию передается временная метка. 
+    """
     ...
 
 
 def check_response(response):
+    """
+    Проверяет ответ API на соответствие документации из урока API сервиса.
+    В качестве параметра функция получает ответ API.
+    Приведенный к типам данных Python.
+    """
     ...
 
 
 def parse_status(homework):
+    """
+    Извлекает из информации о конкретной домашней работе статус этой работы.
+    В качестве параметра функция получает только один элемент из списка ДР.
+    """
     ...
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -43,7 +68,6 @@ def parse_status(homework):
 
 def main():
     """Основная логика работы бота."""
-
     ...
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
